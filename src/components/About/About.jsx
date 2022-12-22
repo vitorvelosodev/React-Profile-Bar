b2573995282e16f3e53119c1c3c14ef24088a0cc
Vitor Veloso <vitor.asv@gmail.com>
2022-12-22 15:36:09 -0300

    Changed text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,7 +6,7 @@ export function About() {
             <h3>About</h3>
             <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
             <h3>Interests</h3>
-            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p>Music scholar. Internet fanatic. Entrepreneur. Digital nomad. Creative.</p>
         </div>
     );
 }
