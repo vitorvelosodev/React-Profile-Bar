4ee77c7ca93f3a947f02c066acd406c054d9ed8a
Vitor Veloso <vitor.asv@gmail.com>
2022-12-22 14:24:43 -0300

    Correction os github icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,16 @@
 import twitter from './../../assets/Twitter-Icon.png'
 import facebook from './../../assets/Facebook-Icon.png'
 import instagram from './../../assets/Instagram-Icon.png'
-import github from './../../assets/Github-Icon.png'
+import github from './../../assets/GitHub-Icon.png'
 import './Footer.css'
 
 export function Footer() {
     return (
         <footer>
             <img src={twitter} alt="Twitter logo" />
-            <img src={facebook} alt="Twitter logo" />
-            <img src={instagram} alt="Twitter logo" />
-            <img src={github} alt="Twitter logo" />
+            <img src={facebook} alt="Facebook logo" />
+            <img src={instagram} alt="Instagram logo" />
+            <img src={github} alt="Github logo" />
         </footer>
     )
 }
